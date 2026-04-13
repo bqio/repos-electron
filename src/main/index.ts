975a03bc51b9847b60d5717682698f04dd263179
bqio <dmitry.mikhaltsov98@gmail.com>
2026-04-13 15:27:47 +0300

    add log

--- src/main/index.ts
+++ src/main/index.ts
@@ -5,8 +5,18 @@ import { bootstrapApp } from '@main/bootstrap'
 import { registerTorrentIpc } from '@main/ipc/torrent'
 import { registerStorageIpc } from '@main/ipc/storage'
 import { registerWindowIpc } from '@main/ipc/window'
+import log from 'electron-log'
 
 const APP_USER_MODEL_ID = 'fun.repos.app'
+
+log.transports.file.level = 'info'
+log.transports.console.level = 'debug'
+
+console.log = (...args: any) => log.info(...args)
+console.info = (...args: any) => log.info(...args)
+console.warn = (...args: any) => log.warn(...args)
+console.error = (...args: any) => log.error(...args)
+console.debug = (...args: any) => log.debug(...args)
 
 app
   .whenReady()
